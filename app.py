73b1f00854f4e43fec675eefc16ec00eb990c091
Zhihao Zhao <zhihao.zhao@Zhihaos-MacBook-Pro.local>
2020-03-11 18:18:33 -0400

    update helm secrets

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return os.environ.get('APP_MESSAGE', 'Hello, World!')
+    text = "{}<br><br>Here's my secret: {}".format(
+        os.environ.get('APP_MESSAGE', 'Hello, World!'),
+        os.environ.get('APP_SECRET')
+    )
+    return text
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
